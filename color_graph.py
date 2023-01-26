a8a1eec57c9a82286c8353d986f1d85c388e6e78
Priya <perali2000@gmail.com>
2023-01-26 13:43:35 -0600

    basic count safe zones

--- color_graph.py
+++ color_graph.py
@@ -1,1 +1,20 @@
-# Goal to visually view the safe zones within the enviornment.
+# Goal to visually view the safe zones within the enviornment
+from Environment import *
+from EscortProblem import *
+
+
+
+
+escort_prob = EscortProblem("Envs/rooms.json", (10.0, 16.667), (100, 45))
+print("HERE")
+
+state = escort_prob.environment.get_starting_state(escort_prob.start_pos)
+safezones = state.safezones
+num = 0
+for poly in safezones.polygons:
+    num += 1
+
+escort_prob.environment.draw_state(state)
+plt.show()
+print("Here")
+print(num)
