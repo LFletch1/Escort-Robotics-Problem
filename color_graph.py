663f9fe5a9badfa341d372d1d9de0d9ada3b7d3e
Priya <perali2000@gmail.com>
2023-01-26 13:54:31 -0600

    minor changes

--- color_graph.py
+++ color_graph.py
@@ -3,18 +3,17 @@ from Environment import *
 from EscortProblem import *
 
 
+def count_safe_zones(envstate):
+    safezones = envstate.safezones
+    num = 0
+    for poly in safezones.polygons:
+        num += 1
+    return num
 
 
 escort_prob = EscortProblem("Envs/rooms.json", (10.0, 16.667), (100, 45))
-print("HERE")
-
 state = escort_prob.environment.get_starting_state(escort_prob.start_pos)
-safezones = state.safezones
-num = 0
-for poly in safezones.polygons:
-    num += 1
-
+number = count_safe_zones(state)
+print(number)
 escort_prob.environment.draw_state(state)
 plt.show()
-print("Here")
-print(num)
