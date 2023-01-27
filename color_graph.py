3d1d2e04f42d320ca9c9deaca62686cf839f656f
Priya <perali2000@gmail.com>
2023-01-26 19:21:06 -0600

    grid point for one point, have not tested

--- color_graph.py
+++ color_graph.py
@@ -2,6 +2,24 @@
 from Environment import *
 from EscortProblem import *
 
+def get_grid_point(start_pos, interval, gp):
+    # Plan:
+    # get all neighbors of single point
+    # check if they are within the enviornment, if not remove
+    # for each remaining neighbor, repeat the same process
+    # Return when the entire list traversed
+    neighbors = [
+                (start_pos[0]- interval, start_pos[1]),
+                (start_pos[0]+ interval, start_pos[1]),
+                (start_pos[0], start_pos[1]-interval),
+                (start_pos[0], start_pos[1]+interval)
+                ]
+
+    for n in neighbors:
+        if not gp.contains(n):
+            neighbors.remove(n)
+
+    return neighbors
 
 def count_safe_zones(envstate):
     safezones = envstate.safezones
@@ -12,8 +30,8 @@ def count_safe_zones(envstate):
 
 
 escort_prob = EscortProblem("Envs/rooms.json", (10.0, 16.667), (100, 45))
+
 state = escort_prob.environment.get_starting_state(escort_prob.start_pos)
 number = count_safe_zones(state)
-print(number)
 escort_prob.environment.draw_state(state)
 plt.show()
